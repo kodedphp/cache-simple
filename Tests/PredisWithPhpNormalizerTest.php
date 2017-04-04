f0a5b622c75275aea231d03bd5dc612e6e1077aa
Mihail Binev <kodeart@gmail.com>
2017-04-04 17:54:40 +0200

    Travis CI cannot handle well Predis client, trying out with bigger sleep()

--- Tests/PredisWithPhpNormalizerTest.php
+++ Tests/PredisWithPhpNormalizerTest.php
@@ -10,6 +10,19 @@ class PredisWithPhpNormalizerTest extends TestCase
 
     use SimpleCacheTestCaseTrait;
 
+    public function test_expired_cache()
+    {
+        $this->assertTrue($this->cache->set('foo', 'bar', 1));
+
+        // at this point the cached item exists
+        $this->assertTrue($this->cache->has('foo'));
+
+        // but after some time it is deleted
+        sleep(3);
+        $this->assertSame('expired', $this->cache->get('foo', 'expired'));
+        $this->assertFalse($this->cache->has('foo'));
+    }
+
     protected function setUp()
     {
         putenv('CACHE_CLIENT=predis');
