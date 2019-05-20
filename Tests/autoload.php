45a9b9455197cfebd84ce60558d6743ea35e6e76
Mihail Binev <kodeart@gmail.com>
2019-05-20 15:44:23 +0200

    - cleanup

--- Tests/autoload.php
+++ Tests/autoload.php
@@ -2,7 +2,5 @@
 
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../vendor/koded/stdlib/functions-dev.php';
 require_once __DIR__ . '/../vendor/autoload.php';
-
 require_once  __DIR__ . '/SimpleCacheTestCaseTrait.php';
