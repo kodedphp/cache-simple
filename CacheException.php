4c88ae2ceda09540fe1e8cb852b1904cc852da49
Mihail Binev <kodeart@gmail.com>
2021-05-03 13:27:53 +0200

    - updates (for stdlib changes)

--- CacheException.php
+++ CacheException.php
@@ -16,7 +16,7 @@ use Throwable;
 
 class CacheException extends KodedException implements InvalidArgumentException
 {
-    protected $messages = [
+    protected array $messages = [
         Cache::E_INVALID_KEY => 'The cache key is invalid, given (:type) :key',
         Cache::E_UNSUPPORTED_LOGGER => 'The cache logger should be NULL or an instance of :supported, given :given',
         Cache::E_DIRECTORY_NOT_CREATED => 'Failed to create a cache directory ":dir"',
@@ -25,37 +25,33 @@ class CacheException extends KodedException implements InvalidArgumentException
         Cache::E_UNSUPPORTED_CLIENT => '[Cache Exception] Unsupported cache client :name',
     ];
 
-    public static function forInvalidKey($key)
+    public static function forInvalidKey($key): static
     {
-        return new self(Cache::E_INVALID_KEY, [':key' => var_export($key, true), ':type' => gettype($key)]);
+        return new static(Cache::E_INVALID_KEY, [':key' => var_export($key, true), ':type' => gettype($key)]);
     }
 
-
-    public static function forUnsupportedLogger(string $supported, string $given)
+    public static function forUnsupportedLogger(string $supported, string $given): static
     {
-        return new self(Cache::E_UNSUPPORTED_LOGGER, [':supported' => $supported, ':given' => $given]);
+        return new static(Cache::E_UNSUPPORTED_LOGGER, [':supported' => $supported, ':given' => $given]);
     }
 
-
-    public static function forCreatingDirectory(string $directory)
+    public static function forCreatingDirectory(string $directory): static
     {
-        return new self(Cache::E_DIRECTORY_NOT_CREATED, [':dir' => $directory]);
+        return new static(Cache::E_DIRECTORY_NOT_CREATED, [':dir' => $directory]);
     }
 
-
-    public static function generic(string $message, Throwable $previous = null)
+    public static function generic(string $message, Throwable $previous = null): static
     {
-        return new self(Cache::E_PHP_EXCEPTION, [':message' => $message], $previous);
+        return new static(Cache::E_PHP_EXCEPTION, [':message' => $message], $previous);
     }
 
-
-    public static function withConnectionErrorFor(string $clientName)
+    public static function withConnectionErrorFor(string $clientName): static
     {
-        return new self(Cache::E_CONNECTION_ERROR, [':client' => $clientName]);
+        return new static(Cache::E_CONNECTION_ERROR, [':client' => $clientName]);
     }
 
-    public static function forUnsupportedClient(string $client)
+    public static function forUnsupportedClient(string $client): static
     {
-        return new self(Cache::E_UNSUPPORTED_CLIENT, [':name' => $client]);
+        return new static(Cache::E_UNSUPPORTED_CLIENT, [':name' => $client]);
     }
 }
